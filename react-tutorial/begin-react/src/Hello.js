e8e33e576554fd0e0254dd9f8f0eb51bb02f48d6
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2020-07-28 23:47:43 +0900

    React 입문 - 07
    - 조건부 렌더링

--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-function Hello({color, name}) {
-    return <div style={{
-        color: color,
-    }}>안녕하세요 {name}</div>;
+function Hello({color, name, isSpecial, booleanDefaultIsTrue}) {
+    console.log(booleanDefaultIsTrue);
+    return (
+        <div style={{
+            color: color,
+        }}>
+            {isSpecial ? <b>*</b> : null}
+            {/* isSpecial && <b>*</b> */}
+            안녕하세요 {name}
+        </div>
+    );
 }
 
 Hello.defaultProps = {
