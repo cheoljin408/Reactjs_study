c18374c678ee719be278f8e2b22fc342dc5c3568
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2020-07-29 18:26:11 +0900

    React 입문 - 15
    - 배열의 항목 제거하기

--- react-tutorial/begin-react/src/UserList.js
+++ react-tutorial/begin-react/src/UserList.js
@@ -1,22 +1,30 @@
 import React from 'react';
 
 // 하나의 컴포넌트에서 여러개의 컴포넌트를 만들 수 있다.
-function User({ user }) {
+function User({ user, onRemove }) {
+    const {username, email, id} = user;
     return (
         <div>
-            <b>{user.username}</b>
-            <span>{user.email}</span>
+            <b>{username}</b>
+            <span>{email}</span>
+            <button onClick={() => onRemove(id)}>삭제</button>
         </div>
     );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove }) {
     return (
         <div>
             {
                 // 배열 내장함수 map을 이용하여 객체형태의 배열인 변수를 컴포넌트 엘리먼트 형태의 배열로 변환을 해준다.
                 users.map(
-                    (user) => (<User user={user} key={user.id} />)
+                    (user) => (
+                        <User 
+                            user={user} 
+                            key={user.id} 
+                            onRemove={onRemove}
+                        />
+                    )
                 )
             }
         </div>
