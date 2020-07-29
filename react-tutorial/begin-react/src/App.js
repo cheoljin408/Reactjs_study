aee2e75ccd088937b2dbc253aa221e327226d830
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2020-07-29 17:50:03 +0900

    React 입문 - 14
    - 배열에 항목 추가하기

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,8 +1,24 @@
-import React, {useRef} from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList.js';
+import CreateUser from './CreateUser.js';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+
+  const {username, email} = inputs;
+
+  const onChange = (e) => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'cheoljin',
@@ -18,17 +34,37 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
 
   const nextId = useRef(4);
 
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    //배열을 복사하는 방법은 두 가지: 스프레드함수 사용, concat 함수 사용
+    setUsers([...users, user]);
+    // setUsers(users.concat(user));
+    setInputs({
+      username: '',
+      email: '',
+    });
     console.log(nextId.current); // 4
     nextId.current += 1;
   };
 
   return (
-    <UserList users={users} />
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
   );
 }
 
