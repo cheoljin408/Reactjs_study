f6de25dceeceb5e3c6ec2046ad89ccbaebdb37c5
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2020-08-03 15:48:03 +0900

    React 입문 - 27
    - 클래스형 컴포넌트 소개

--- react-tutorial/begin-react/src/Hello.js
+++ react-tutorial/begin-react/src/Hello.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React ,{ Component }from 'react';
 
-function Hello({color, name, isSpecial, booleanDefaultIsTrue}) {
-    console.log(booleanDefaultIsTrue);
-    return (
-        <div style={{
-            color: color,
-        }}>
-            {isSpecial ? <b>*</b> : null}
-            {/* isSpecial && <b>*</b> */}
-            안녕하세요 {name}
-        </div>
-    );
+// Hello 컴포넌트를 class 컴포넌트로 만들어 보기
+class Hello extends Component {
+    static defaultProps = {
+        name: '이름없음'
+    };
+    
+    render () {
+        const { color, isSpecial, name } = this.props;
+        return (
+            <div style={{color}}>
+                {isSpecial && <b>*</b>}
+                안녕하세요 {name}
+            </div>
+        );
+    }
 }
 
-Hello.defaultProps = {
-    name: 'No Name'
-};
+// function Hello({color, name, isSpecial, booleanDefaultIsTrue}) {
+//     console.log(booleanDefaultIsTrue);
+//     return (
+//         <div style={{
+//             color: color,
+//         }}>
+//             {isSpecial ? <b>*</b> : null}
+//             {/* isSpecial && <b>*</b> */}
+//             안녕하세요 {name}
+//         </div>
+//     );
+// }
+
+// Hello.defaultProps = {
+//     name: 'No Name'
+// };
 
 export default Hello;
