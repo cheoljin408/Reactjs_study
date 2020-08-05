4b30e7b4f5a82b8ad14ab633b1791d4921a649cb
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2020-08-05 19:58:12 +0900

    컴포넌트 스타일링 - 05 Sass 버튼 C - outline, fullWidth props 설정

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -1,29 +1,61 @@
-import React from "react";
-import Button from "./components/Button.js";
-import "./App.scss";
+import React from 'react';
+import Button from './components/Button.js';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-        <div className="buttons">
-            <Button size="large">BUTTON</Button>
-            <Button>BUTTON</Button>
-            <Button size="small">BUTTON</Button>
-        </div>
+    return (
+        <div className="App">
+            <div className="buttons">
+                <Button size="large">BUTTON</Button>
+                <Button>BUTTO N</Button>
+                <Button size="small">BUTTON</Button>
+            </div>
 
-        <div className="buttons">
-            <Button size="large" color="gray">BUTTON</Button>
-            <Button color="gray">BUTTON</Button>
-            <Button size="small" color="gray">BUTTON</Button>
-        </div>
+            <div className="buttons">
+                <Button size="large" color="gray">
+                    BUTTON
+                </Button>
+                <Button color="gray">BUTTON</Button>
+                <Button size="small" color="gray">
+                    BUTTON
+                </Button>
+            </div>
 
-        <div className="buttons">
-            <Button size="large" color="pink">BUTTON</Button>
-            <Button color="pink">BUTTON</Button>
-            <Button size="small" color="pink">BUTTON</Button>
+            <div className="buttons">
+                <Button size="large" color="pink">
+                    BUTTON
+                </Button>
+                <Button color="pink">BUTTON</Button>
+                <Button size="small" color="pink">
+                    BUTTON
+                </Button>
+            </div>
+
+            <div className="buttons">
+                <Button size="large" outline={true}>
+                    BUTTON
+                </Button>
+                <Button color="gray" outline={true}>
+                    BUTTON
+                </Button>
+                <Button size="small" color="pink" outline>
+                    BUTTON
+                </Button>
+            </div>
+
+            <div className="buttons">
+                <Button size="large" fullWidth={true}>
+                    BUTTON
+                </Button>
+                <Button size="large" color="gray" fullWidth={true}>
+                    BUTTON
+                </Button>
+                <Button size="large" color="pink" fullWidth>
+                    BUTTON
+                </Button>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
