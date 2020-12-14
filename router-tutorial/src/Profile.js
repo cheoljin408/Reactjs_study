905895adc5c8632b61fd34b3ad3d72c4d2a2d130
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2020-12-14 23:55:25 +0900

    리액트 라우터 - 03 서브라우트

--- router-tutorial/src/Profile.js
+++ router-tutorial/src/Profile.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// 프로필에서 사용 할 데이터
 const profileData = {
     velopert: {
         name: '김민준',
@@ -14,7 +13,6 @@ const profileData = {
 };
 
 const Profile = ({ match }) => {
-    // 파라미터를 받아올 땐 match 안에 들어있는 params 값을 참조합니다.
     const { username } = match.params;
     const profile = profileData[username];
     if (!profile) {
