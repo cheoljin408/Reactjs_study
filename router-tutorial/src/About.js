c9c798a9e328d14318a138e63ed559de6e18d456
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2020-12-14 14:52:25 +0900

    리액트 라우터 - 02 파라미터와 쿼리

--- router-tutorial/src/About.js
+++ router-tutorial/src/About.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true,
+    });
+
+    const detail = query.detail === 'true';
     return (
         <div>
             <h1>소개</h1>
@@ -8,6 +14,7 @@ const About = () => {
                 이 프로젝트는 리액트 라우터 기초를 실습해보는 예제
                 프로젝트랍니다.
             </p>
+            {detail && <p>asdfasdfasdf</p>}
         </div>
     );
 };
